d5d8ba90e3a17ce94d14f417a06eed3ea400332f
Willian <williansilvap25@gmail.com>
2022-07-11 22:36:31 -0300

    install bcrypt jsonwebtoken and mongoose

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require ("dotenv").config();
+
 const express = require("express");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const routes = require("./routes/routes");
+
 const app = express();
 const port = process.env.PORT;
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
-app.listen(port, () => {console.log(`Servidor rodando em: http://localhost:${port}`);});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em: http://localhost:${port}`);
+});
